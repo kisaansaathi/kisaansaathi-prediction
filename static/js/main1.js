af2649018e2e99faf01ff155f4acd12987afffd3
Agrikanti <79264148+Agrikanti@users.noreply.github.com>
2021-10-10 19:17:49 +0530

    Add files via upload

---
+++ static/js/main1.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+    // Init
+    $('.image-section').hide();
+    $('.loader').hide();
+    $('#result1').hide();
+
+    // Upload Preview
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview1').css('background-image', 'url(' + e.target.result + ')');
+                $('#imagePreview1').hide();
+                $('#imagePreview1').fadeIn(650);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#imageUpload1").change(function () {
+        $('.image-section').show();
+        $('#btn-predict1').fadeIn();
+        $('#result1').text('');
+        $('#result1').hide();
+        readURL(this);
+    });
+
+    // Predict
+    $('#btn-predict1').click(function () {
+        var form_data = new FormData($('#upload-file1')[0]);
+        var cropName = $('#crop-choose1').find(":selected").text();
+        var url='';
+        // Show loading animation
+        if(cropName=="Maize"){
+          url= '/maize_disease';
+        }
+        if(cropName=="Rice"){
+          url= '/rice_disease';
+        }
+        $(this).hide();
+        $('.loader').show();
+
+        // Make prediction by calling api /predict
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            async: true,
+            success: function (data) {
+                // Get and display the result
+                $('.loader').hide();
+                $('#result1').fadeIn(300);
+                $('#result1').text(' Result:  ' + data);
+                console.log('Success!');
+            },
+        });
+    });
+
+});
