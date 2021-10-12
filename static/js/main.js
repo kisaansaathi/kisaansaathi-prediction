f1257b8015ab936d34e4f25c8b722f93a7bc288f
Agrikanti <79264148+Agrikanti@users.noreply.github.com>
2021-10-12 14:49:45 +0530

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -51,7 +51,7 @@ $(document).ready(function () {
             success: function (data) {
                 // Get and display the result
                 $('.loader').hide();
-                $('#result').fadeIn(600);
+                $('#result').fadeIn(300);
                 $('#result').text(' Result:  ' + data);
                 console.log('Success!');
             },
